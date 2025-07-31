c1fd406968eabc444957467f62d5b8febb16b628
silviosampaio7 <88442130+silviosampaio7@users.noreply.github.com>
2025-07-31 19:54:36 -0400

    Update streamlit-dash.py

--- streamlit-dash.py
+++ streamlit-dash.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+from plotly import express as px
 # Configuração da página
 st.set_page_config(page_title="DESPESAS", layout="wide")
 # Título da aplicação
